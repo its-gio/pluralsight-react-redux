86d7134d1e646c08e8798dd4a675dafe13af9514
Giovani Aguirre <me@its-g.io>
2020-04-14 16:27:44 -0700

    Created form with changes and submit functions.

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import { Button, Input } from "@material-ui/core";
 
 export default class Courses extends Component {
+  state = {
+    course: {
+      title: "",
+    },
+  };
+
+  handleChange = (e) => {
+    this.setState({ course: { [e.target.name]: e.target.value } });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Testing!");
+  };
+
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
-      </div>
+        <h4>Add Course</h4>
+        <Input
+          placeholder="Title"
+          inputProps={{ "aria-label": "description" }}
+          value={this.state.course.title}
+          onChange={this.handleChange}
+          name="title"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Save
+        </Button>
+      </form>
     );
   }
 }
