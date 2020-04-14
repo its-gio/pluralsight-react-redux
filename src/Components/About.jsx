720b46c8c8f3782f1256ec49c728e4a4347b67df
Giovani Aguirre <me@its-g.io>
2020-04-14 11:55:15 -0700

    Content for about added.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Container } from '@material-ui/core'
 
 export default class About extends Component {
   render() {
     return (
-      <div>
-        About!
-      </div>
+      <Container className="landing">
+        <h2>Technologies Used</h2>
+        <p>react, redux, react-router, and material-ui</p>
+      </Container>
     )
   }
 }
